61cd0f497998d505754454f1f4f9ea50bd2d5367
jessepiza <jessenia.piza17@gmail.com>
2019-11-03 10:05:27 -0500

    'Actualizado'

--- funciones2.cpp
+++ funciones2.cpp
@@ -5,28 +5,28 @@
 
 using namespace std;
 
-string signo(string str){
-  string signo = "";
+int signo(string str){
+  int signo = 0;
   if (str.find('[') == string::npos){
     if(str.find('*') != string::npos)
-      signo = '*';
+      signo = str.find('*');
     else if (str.find('/') != string::npos)
-      signo = '/';
+      signo = str.find('/');
     else if (str.find('^') != string::npos)
-      signo = '^';
+      signo = str.find('^');
     else if (str.find('+') != string::npos)
-      signo = '+';
+      signo = str.find('+');
     else if (str.find('-') != string::npos)
-      signo = '-';
+      signo = str.find('-');
   }
 
   else{
     for (unsigned int i = 1; i < str.size();i++){
       if (str[i] == ']'){
-        signo = str[i+1];
+        signo = i+1;
         break;
       }else if(str[i] == '['){
-        signo = str[i-1];
+        signo = i-1;
         break;
       }
     }
@@ -34,27 +34,38 @@ string signo(string str){
   return signo;
 }
 
+bool conver(string str){
+  for (unsigned int q = 0; q < str.size(); q++){
+    if (!isalnum(str[q])){
+      return false;
+    }
+  }
+  return true;
+}
 
 string polaca(string str){
-  string s = signo(str);
-  // cout << s << endl;
-  // cout << "De 0 a S" << str.substr(0, str.find(s)) << endl;
-  // cout << "De S al final" << str.substr(str.find(s)+1, str.size()) << endl;
+  string total;
+  int s = signo(str);
+  if (conver(str)){
+    return str;
+  }
   if (str.size() <= 1){
     return str;
   }
   else{
-    string parte1 = str.substr(0, str.find(s));
-    string parte2 = str.substr(str.find(s)+1, str.size());
-    if ((parte1[0] == '[') and (parte2[0] != '['))
-      return s + "]" + polaca(str.substr(1, str.find(s)-2)) + "[" + "]" + polaca(str.substr(str.find(s)+1, str.size())) + "[";
-    else if ((parte2[0] == '[') and (parte1[0] != '['))
-      return s + "]" + polaca(str.substr(0, str.find(s))) + "[" + "]" + polaca(str.substr(str.find(s)+2, str.size()-4)) + "[";
-    else if ((parte2[0] == '[') and (parte1[0] == '['))
-      return s + "]" + polaca(str.substr(1, str.find(s)-2)) + "[" + "]" + polaca(str.substr(str.find(s)+2, str.size()-(str.size()-3))) + "[";
-    else
-      return s + "]" + polaca(str.substr(0, str.find(s))) +"[" +"]" + polaca(str.substr(str.find(s)+1, str.size())) + "[";
-  }
+    total += str[s];
+    string parte1 = str.substr(0, s);
+    string parte2 = str.substr(s+1, str.size());
+    if ((parte1[0] == '[') and (parte2[0] != '[')){
+      total += "[" + polaca(str.substr(1, s-2)) + "]" + "[" + polaca(str.substr(s+1, parte2.size())) + "]";}
+    else if ((parte2[0] == '[') and (parte1[0] != '[')){
+      total += "[" + polaca(str.substr(0, s)) + "]" + "[" + polaca(str.substr(s+2, parte2.size()-2)) + "]";}
+    else if ((parte2[0] == '[') and (parte1[0] == '[')){
+      total += "[" + polaca(str.substr(1, s-2)) + "]" + "[" + polaca(str.substr(s+2, parte2.size()-2)) + "]";}
+    else {
+      total += "[" + polaca(parte1) +"]" +"[" + polaca(parte2) + "]";}
+    }
+  return total;
 }
 
 string polaca_inv(string str){
@@ -67,7 +78,7 @@ string polaca_inv(string str){
 }
 
 int main(){
-    string str = "[x+2]^2";
-    cout<< polaca_inv(str)<<endl;
+    string str = "[x+225]+22*x";
+    cout<< polaca(str)<<endl;
     return 0;
 }
