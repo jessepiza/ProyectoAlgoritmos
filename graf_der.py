0a1aab4d7b4167d044fdd6f6234ac7439ddf40c8
laurasalazar44 <alesalazar-44@hotmail.com>
2019-11-20 17:56:10 -0500

    Gráfico

--- graf_der.py
+++ graf_der.py
@@ -1,4 +1,6 @@
 from tkinter import *
+import subprocess
+import os
 
 raiz = Tk()
 width = 1000
@@ -8,21 +10,25 @@ micanvas = Canvas (raiz, bg ="lightblue", width = width, height = height)
 micanvas.pack()
 micanvas.create_text(width/2, 100, text = "Ingrese la función que desea derivar", anchor = CENTER, font =("Purisa", "30"))
 
-
-def fun_ini():
-    raiz.config (width = 1000, height = 800)
+def der_fun():
+    raiz.config (width = 1000, height = 600)
     e = Entry(raiz,font=('Arial',30))
     e.place(relx=0.5,rely=0.5,anchor='s',relheight=0.15,relwidth=0.4)
-
-def der_fun():
-    fun_ini()
-    der = Button (raiz, width = 5, height = 2, anchor = CENTER, text = "Derivar", command = None, font =("Arial", "15"))
+    a = e.get()
+    der = Button (raiz, width = 5, height = 2, anchor = CENTER, text = "Derivar", command = lambda: txt(e), font =("Arial", "15"))
     micanvas.create_window(500,375 , window = der, anchor = CENTER)
-    raiz.update()
+
+    # subprocess.run(["/home/lovelace/ProyectoAlgoritmos/filename.txt"])
 
 def limpiar():
     micanvas.delete("all")
 
+def txt(e):
+    e.get()
+    file = open("/home/lovelace/ProyectoAlgoritmos/filename.txt", "w")
+    file.write(str(e.get()) + os.linesep)
+    file.close()
+    print(e.get())
 
-fun_ini()
+der_fun()
 raiz.mainloop()
